52e994c3ea5ec2e3aa153e5279fd93a3ee575a0e
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-16 21:46:22 +0100

    add landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Landing = (props) => {
+  return (
+    <div className="landing">
+      <h1 className="landing__title">Pet Personalities</h1>
+      <p className="landing__text">Find out more about differnt type of pets! <br/>
+      ...and share your expierience</p>
+    </div>
+  );
+};
+
+export default Landing;
