05d81625ba01d7f353a8c8ebd3f4d89b3062c8fc
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-19 12:08:34 +0100

    simplify userschema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,32 +2,22 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-      },
-    secondName: {
-        type: String,
-      },
-    userName: {
+    name: {
         type: String,
         required: [true, 'Username is required'],
         unique: true,
       },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-      },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-      },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        defualt: false,
-      },
-  }, {
+    // email: {
+    //     type: String,
+    //     required: [true, 'Email is required'],
+    //     unique: true,
+    //   },
+    // password: {
+    //     type: String,
+    //     required: [true, 'Password is required'],
+    //   },
+  },
+   {
     timestamps: true,
   });
 
