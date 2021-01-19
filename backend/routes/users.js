61fe255d6e929db864a04d87403bfb09602491f9
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-19 21:46:22 +0100

    backend boilerplate

---
+++ backend/routes/users.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import User from '../models/userSchema.js';
+
+const router = express.Router();
+
+//@route GET api/users
+//@desc GET All users
+//@access Public
+router.get('/', (req, res)=> {
+  User.find()
+  .then(users =>res.json(users));
+});
+
+//@route POST api/users
+//@desc Create an user
+//@access Public
+router.post('/', (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+  });
+  newUser.save().then(user=>res.json(user));
+});
+
+//@route DELETE api/users/:id
+//@desc DELETE a user
+//@access Public
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id)
+  .then(user => user.remove()
+  .then(()=> res.json({ success: true })))
+  .catch(err=> res.status(404).json({ success: false }));
+})
+;
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import db from './config/db.js';
+import users from './routes/users.js';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+//connect to mongodb
+
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
+.then(()=> console.log('Mongo Connected...'))
+.catch(err => console.log(err));
+
+//use routes
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
