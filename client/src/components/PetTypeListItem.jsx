eba0b36bbecb9eee0bac7c41a7e631b4e512363a
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-19 21:45:03 +0100

    create pettype list items

--- client/src/components/PetTypeListItem.jsx
+++ client/src/components/PetTypeListItem.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const PetTypeListItem = ({ id, type, img }) => {
   return (
     <div className="petTypeListItem">
-      <h2 className="petTypeListItem__title"></h2>
+      <h2 className="petTypeListItem__title">{type}</h2>
       <p className="petTypeListItem__desc"></p>
-      <img src="" alt="" className="petTypeListItem__img"/>
+      <div className="petTypeListItem__img-wrap">
+        <img src={img} alt={type} className="petTypeListItem__img"/>
+      </div>
     </div>
   );
 };
