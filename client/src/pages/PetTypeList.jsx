6f35746cc0570ccafee0eeeee5993e98bf1016c6
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-22 11:20:37 +0100

    get types from backend

--- client/src/pages/PetTypeList.jsx
+++ client/src/pages/PetTypeList.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import PetTypeListItem from '../components/PetTypeListItem';
+import { DataContext } from '../context/DataContext';
 
 const PetTypeList = () => {
-  const petTypes = [{ id: 1, type: 'dogs', img: 'https://www.cheatsheet.com/wp-content/uploads/2017/10/corgi-dog-puppies.jpg' },
-     { id: 2, type: 'cats', img: 'https://www.rover.com/blog/wp-content/uploads/2019/02/multiple_cats.jpg' },
-     { id: 3, type: 'small animals', img: 'https://www.petlandwichita.com/wp-content/uploads/1970/01/small-animals-1.jpg' },
-     { id: 4, type: 'reptiles', img: 'https://www.petlandwichita.com/wp-content/uploads/1970/01/small-animals-1.jpg' },
-     { id: 5, type: 'birds', img: 'https://www.petlandwichita.com/wp-content/uploads/1970/01/small-animals-1.jpg' },
-     { id: 6, type: 'insects', img: 'https://www.petlandwichita.com/wp-content/uploads/1970/01/small-animals-1.jpg' },
-     { id: 7, type: 'fishes', img: 'https://www.petlandwichita.com/wp-content/uploads/1970/01/small-animals-1.jpg' },
-      ];
+  const { types } = useContext(DataContext);
+
   return (
     <Link className="petTypeList">
-{  petTypes.map(petType => {
+{  types.map(petType => {
     return <PetTypeListItem
-    key = {petType.id}
+    key = {petType._id}
     type = {petType.type}
     img= {petType.img} />;}
   )}
