b510f5364a72039cfff6f4a7d2828d88f22c8fc6
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-22 11:19:10 +0100

    add more react routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,39 @@ import { BrowserRouter as Router,
         Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import PetTypeList from './pages/PetTypeList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BreedDetails from './pages/BreedDetails';
 import Navbar from './components/Navbar';
+import DataContextProvider from './context/DataContext';
+import LoginContextProvider from './context/LoginContext';
+
 function App() {
   return (
     <Router>
     <div className="App">
+      <LoginContextProvider>
       <Navbar/>
       <Switch>
       <Route exact path='/'>
-     <Landing/>
-     </Route>
-     <Route path = '/types'>
-       <PetTypeList/>
-     </Route>
+        <Landing/>
+      </Route>
+      <DataContextProvider>
+        <Route path = '/types'>
+          <PetTypeList/>
+        </Route>
+        <Route path = '/breeds'>
+          <BreedDetails/>
+        </Route>
+      </DataContextProvider>
+      <Route path = '/login'>
+        <Login/>
+      </Route>
+      <Route path = '/register'>
+        <Register/>
+      </Route>
      </Switch>
+     </LoginContextProvider>
     </div>
     </Router>
   );
