2e16be77a2ade227bb81fc9463f7a09847ce6846
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-24 20:08:12 +0100

    linter

---
+++ client/src/components/BreedDetailItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BreedDetailItem = () => {
+  return (
+    <div className="breedDetailItem">
+      <h2> Alaskan Malamute </h2>
+      <div className="breedDetailItem__img">
+        <img src="" alt=""/>
+      </div>
+      <ul>
+        <li>Height:</li>
+        <li>Personality:</li>
+        <li>To consider: </li>
+      </ul>
+    </div>
+  );
+};
+
+export default BreedDetailItem;
