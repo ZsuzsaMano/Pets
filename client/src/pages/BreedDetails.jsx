0e473684ba9e09dba26b85fad5983ec875514323
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-25 18:14:13 +0100

    display animal depending on param

--- client/src/pages/BreedDetails.jsx
+++ client/src/pages/BreedDetails.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { DataContext } from '../context/DataContext';
 import BreedDetailItem from '../components/BreedDetailItem';
 
 const BreadDetails = () => {
   const { breeds }  = useContext(DataContext);
+  const { typeParam } = useParams();
   return (
     <div className="breeds">
       {breeds.map(breed=> {
-        return <BreedDetailItem
+        return (breed.type === typeParam) && <BreedDetailItem
           key = {breed._id}
           name={breed.name}
           type = {breed.type}
