d398101187588a5517f99115d192b9a116049a60
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-25 14:37:00 +0100

    update link to breeds

--- client/src/components/PetTypeListItem.jsx
+++ client/src/components/PetTypeListItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const PetTypeListItem = ({ id, type, img }) => {
   return (
     <div className="petTypeListItem">
-    <Link to ={type} className="petTypeListItem__link">
+    <Link to ={'breeds/' + type} className="petTypeListItem__link">
       <h2 className="petTypeListItem__title">{type}</h2>
       <p className="petTypeListItem__desc"></p>
       <div className="petTypeListItem__img-wrap">
