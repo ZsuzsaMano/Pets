edea68622d3f3e07a19af389ee193e6ed691daf6
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-25 14:37:51 +0100

    profile page basic

---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profile = (props) => {
+  return (
+    <div className="profile">
+      <img src="" alt="" className="profile__img"/>
+      <h3 className="profile_name"></h3>
+      <p className="profile__email"></p>
+    </div>
+  );
+};
+
+export default Profile;
