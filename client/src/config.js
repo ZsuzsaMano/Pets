2c7c9b58db52f6048dc5b693e20cf2cc810c87a9
Zsuzsa Lukacs <manoaway@gmail.com>
2021-02-03 13:46:32 +0100

    fix serverURL in config

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ export const config = {
   serverURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:5000"
-      : "https://petpersonalities.herokuapp.com/"
+      : "https://petpersonalities.herokuapp.com"
 };
