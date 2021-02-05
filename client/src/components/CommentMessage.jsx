2fe939a99003337307e113ccff9d3bfe5107389b
Zsuzsa Lukacs <manoaway@gmail.com>
2021-02-05 19:55:08 +0100

    style comments section

--- client/src/components/CommentMessage.jsx
+++ client/src/components/CommentMessage.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LoginContext } from "../context/LoginContext";
 
 import moment from "moment";
 
 const CommentMessage = ({ message, chatName, date }) => {
   const { user } = useContext(LoginContext);
-  const dateFrom = moment(date).fromNow();
+  //const dateFrom = moment(date).fromNow();
   return (
     <div className={chatName === user.name ? " myMessage" : "message"}>
-      <p className="bubble__name">{chatName === user.name ? "Me" : chatName}</p>
-      <p className="bubble__text">{message}</p>
-      <p className="bubble__time">{dateFrom}</p>
+      <div className="message__head">
+        <p className="bubble__name">
+          {chatName === user.name ? "Me" : chatName}
+        </p>
+        <p className="bubble__time">{date}</p>
+      </div>
+      <p className="message__text">{message}</p>
     </div>
   );
 };
--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import CommentMessage from "./CommentMessage";
 
 const Comments = props => {
+  const [showComments, toggleShowComments] = useState(true);
+
+  const toggleComments = () => {
+    toggleShowComments(!showComments);
+  };
+  const messages = [
+    {
+      id: 1,
+      chatName: "dev",
+      date: "2021.01.30",
+      message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },
+    {
+      id: 2,
+      chatName: "dev2",
+      date: "2021.02.04",
+      message: "Lorem ipsum dolor sit amet. Lorem ipsum dolor."
+    },
+    {
+      id: 3,
+      chatName: "dev3",
+      date: "2021.02.05",
+      message: "Lorem ipsum dolor sit amet."
+    },
+    {
+      id: 4,
+      chatName: "dev4",
+      date: "2021.02.06",
+      message:
+        "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    }
+  ];
   return (
     <div className="comments">
-      <p className="open-comments">see comments...</p>
+      <p className="open-comments" onClick={toggleComments}>
+        {showComments ? "hide comments..." : "see comments..."}
+      </p>
+      <div
+        className={
+          showComments ? "messages messages-open" : "messages messages-closed"
+        }
+      >
+        {messages.map(message => (
+          <CommentMessage
+            key={message.id}
+            chatName={message.chatName}
+            date={message.date}
+            message={message.message}
+          />
+        ))}
+      </div>
     </div>
   );
 };
